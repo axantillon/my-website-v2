1ff41d05eb039a6c7ddf8c31c680433fb8fe0a72
Andres Antillon <aantillon.leiva@gmail.com>
2021-10-13 15:18:10 -0600

    disbale zoom on input focus

--- src/utils/interceptors.ts
+++ src/utils/interceptors.ts
@@ -1,22 +1,18 @@
 import {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from "axios";
 
 const onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
-    console.info(`[request] [${JSON.stringify(config)}]`);
     return config;
 }
 
 const onRequestError = (error: AxiosError): Promise<AxiosError> => {
-    console.error(`[request error] [${JSON.stringify(error)}]`);
     return Promise.reject(error);
 }
 
 const onResponse = (response: AxiosResponse): AxiosResponse => {
-    console.info(`[response] [${JSON.stringify(response)}]`);
     return response;
 }
 
 const onResponseError = (error: AxiosError): Promise<AxiosError> => {
-    console.error(`[response error] [${JSON.stringify(error)}]`);
     return Promise.reject(error);
 }
 
